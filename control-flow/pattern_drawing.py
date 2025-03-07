981f7cc30f62b7b86a389a8b7baea168e7d2ea46
Saad Fathy <saad.fathy.abdelkareem@gmail.com>
2025-03-08 01:22:02 +0200

    Personal Daily Reminder

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -3,7 +3,7 @@ number = int(input("Enter the size of the pattern: "))
 count = 1
 
 while count <= number:
-    for x in range(number):
+    for _ in range(number):
         print("*", end="")
     count += 1
     print()
