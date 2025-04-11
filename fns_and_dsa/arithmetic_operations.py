4b4ff534274f6c567092801f00ffb5d29647a178
Saad Fathy <saad.fathy.abdelkareem@gmail.com>
2025-04-12 00:12:55 +0200

    new arithmetic function

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,17 +1,13 @@
-def perform_operation(num1,num2,operation):
-       
-    if operation == 'add':
-            return num1 + num2
-    elif operation == 'subtract':
-        return num1 - num2
-    elif operation == 'multiply':
-        return num1 * num2
-    elif operation == 'divide':
-        if num2 == 0:
+def perform_operation(x, y, z):
+    
+    if z == 'add':
+        return x + y
+    elif z == 'subtract':
+        return x - y
+    elif z == 'multiply':
+        return x * y
+    elif z == 'divide':
+        if y == 0:
             return "Division by zero is not allowed"
         else:
-            return num1 / num2
-    else:
-        return "Invalid operation"
-
-perform_operation(5,4,'add')
+            return x / y
