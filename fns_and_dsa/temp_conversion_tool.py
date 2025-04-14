df9aee91d9e79c0e8e9563f05788dcf4580c325e
Saad Fathy <saad.fathy.abdelkareem@gmail.com>
2025-04-14 07:24:35 +0200

    Temperature conversion

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -6,19 +6,19 @@ Unit = input("Is this temperature in Celsius or Fahrenheit? (C/F): ").strip().up
 
 def convert_to_celsius():
     if type(Temp) is float:
-        print(f"{Temp} °F is {((Temp-32)*FAHRENHEIT_TO_CELSIUS_FACTOR):.1f} °C")
+        return (f"{Temp} °F is {((Temp-32)*FAHRENHEIT_TO_CELSIUS_FACTOR):.1f} °C")
     else:
         print(f"Invalid temperature. Please enter a numeric value.")
         
 def convert_to_fahrenheit():
     if type(Temp) is float:
-        print(f"{Temp} °C is {(CELSIUS_TO_FAHRENHEIT_FACTOR*Temp+32):.1f} °F")
+        return (f"{Temp} °C is {(CELSIUS_TO_FAHRENHEIT_FACTOR*Temp+32):.1f} °F")
     else:
         print(f"Invalid temperature. Please enter a numeric value.")
 
 if Unit =="F":
-    convert_to_celsius()
+    print(convert_to_celsius())
 
 elif Unit == "C":
-    convert_to_fahrenheit()
+    print(convert_to_fahrenheit())
 
