5fb1015e25f9f3f27752353fe5e71d81e424796e
Saad Fathy <saad.fathy.abdelkareem@gmail.com>
2025-08-11 23:39:39 +0300

    bank_account

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,5 +1,5 @@
 class BankAccount:
-    def __init__(self,account_balance=40):
+    def __init__(self,account_balance=0):
         self.balance = account_balance
         
     def deposit(self, amount):
@@ -8,8 +8,9 @@ class BankAccount:
         
     def withdraw(self, amount):
         if amount > 0 and self.balance - amount >= 0:
+            return True
             self.balance -= amount
-            
+
     def display_balance(self):
         print(f"Current Balance:{self.balance}")
         
