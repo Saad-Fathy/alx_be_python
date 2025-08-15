b6eba4c3e8b9f5e36e93101227c41775bc2cb142
Saad Fathy <saad.fathy.abdelkareem@gmail.com>
2025-08-15 21:43:47 +0300

    Library_management

--- programming_paradigm/library_management.py
+++ programming_paradigm/library_management.py
@@ -4,12 +4,6 @@ class Book:
         self.author = author
         self._is_checked_out = False
         
-        
-    def return_book(self, title):
-        for book in self._books:
-            if book.title == title:
-                book._is_checked_out = False
-                
 class Library:
     def __init__(self):
         self._books = []
@@ -25,9 +19,16 @@ class Library:
             if book.title == title:
                 if not book._is_checked_out:
                     book._is_checked_out = True
-                    return f"Book {title} has been check out"
+                    return f"Book {title} has been checked out"
                 else:
                     return f"Book {title} is already checked out"
-            else:
-                return f"Book {title} not found in the library"
+        return f"Book {title} not found in the library"
+            
+    def return_book(self, title):
+        for book in self._books:
+            if book.title == title:
+                if book._is_checked_out
+                    book._is_checked_out = False
+                    return f"Book {title} has been returned"
+        return f"Book {title} not found in the library"
             
